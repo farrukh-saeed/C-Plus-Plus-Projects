bb6da88aad3609cc30503e54c51e7249dfbdaf17
Farrukh Saeed <17348324+farrukh-saeed@users.noreply.github.com>
2022-01-31 14:20:06 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Additon-Project/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int x, y;
+ cout << "Enter an integer number : ";
+ cin >> x;
+y = x * x;
+ cout << "Double that number is : " << y ;
+return 0 ;
+
+}
